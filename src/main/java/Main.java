5ee741ea5f20421ae4053bda1e4f325a8db27986
李柏强3120004696 <000000000@11.com>
2022-09-14 15:54:54 +0800

    complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,24 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
+
+import com.utils.FileUtil;
+import com.utils.IOUtil;
+
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) {
-        try {
-            File file = new File("D:\\其他\\Learning\\大三\\上\\软件工程\\第二次作业\\测试文本\\orig.txt");
+    public static void main(String[] args) throws IOException {
 
-            if (file.exists() && file.isFile()) {
-                InputStreamReader reader = new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8);
-                BufferedReader bufferedReader = new BufferedReader(reader);
-                String lineTxt = null;
-                List<String> txtList = new ArrayList<>();
-                while ((lineTxt = bufferedReader.readLine()) != null) {
-                    txtList.add(lineTxt);
-                    System.out.println(lineTxt);
-                }
-                reader.close();
-            }
-
-            System.out.println();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        //file1为初始文件路径
+        String file1 = args[0];
+        //file2为抄袭文件路径
+        String file2 = args[1];
+        Map<String, List<Integer>> stringListMap = IOUtil.word(file1);
+        Map<String, List<Integer>> stringListMap2 = IOUtil.word(file2);
+        Double aDouble = IOUtil.count(stringListMap, stringListMap2);
+        System.out.println(aDouble);
+        //args[2]为输出文件路径
+        FileUtil.writeFile(args[2], String.valueOf(aDouble));
 
     }
 
--- src/main/java/com/utils/FileUtil.java
+++ src/main/java/com/utils/FileUtil.java
@@ -2,19 +2,21 @@ package com.utils;
 
 import com.exception.FileException;
 
+
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
+
+import java.io.*;
+
 
 public class FileUtil {
     /**
      * 读取路径
      *
      * @param filepath 文件路径
-     * @return 结果
      * @throws FileException 异常
      */
-    public static String readFile(String filepath) throws FileException {
+    public static void readFile(String filepath) throws FileException {
         if (filepath == null || "".equals(filepath)) {
             throw new FileException("文件路径为空");
         }
@@ -26,7 +28,6 @@ public class FileUtil {
         }
         String result = String.valueOf(fileReader);
         System.out.println(result);
-        return result;
     }
 
     /**
@@ -34,16 +35,20 @@ public class FileUtil {
      *
      * @param filepath 文件路径
      * @param content  结果
-     * @return true
      * @throws IOException 异常
      */
-    public static Boolean writeFile(String filepath, String content) throws IOException {
-        FileWriter writer = new FileWriter(filepath);
-        try {
-            writer.write(content);
-        } catch (Exception e) {
-            System.out.println("答案文件输出异常，结果为：" + content);
-        }
-        return true;
+    public static void writeFile(String filepath, String content) throws IOException {
+        //指定文件
+        File f = new File(filepath);
+        //创建输出流fos并以f为参数
+        FileOutputStream fos = new FileOutputStream(f);
+        //创建字符输出流对象osw并以fos为参数
+        OutputStreamWriter osw = new OutputStreamWriter(fos);
+        //创建一个带缓冲的输出流对象bw，并以osw为参数
+        BufferedWriter bw = new BufferedWriter(osw);
+        //使用bw写入一行文字，为字符串形式String
+        bw.write(content);
+        //关闭并保存
+        bw.close();
     }
 }
